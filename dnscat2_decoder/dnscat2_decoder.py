75a7179c460565e4dd3b5a1dfc5f995a9be0c510
chris <chris@local.lan>
2024-03-17 17:52:39 +0000

    check for msg_type -> faster

--- dnscat2_decoder/dnscat2_decoder.py
+++ dnscat2_decoder/dnscat2_decoder.py
@@ -14,6 +14,7 @@ def get_args():
 
 def extract_c2c(src_h, dst_h, m_domain, pcap_file):
     capture = PcapNgReader(pcap_file)
+    msg_type_msg = 1
     
     c2c = []
     for pkt in capture:
@@ -23,10 +24,31 @@ def extract_c2c(src_h, dst_h, m_domain, pcap_file):
             if m_domain in pkt['DNSQR'].qname.decode():
                # remove ctlr bytes, newlines and .
                # see: https://github.com/iagox86/dnscat2/blob/master/doc/protocol.md#messages
-               # and https://github.com/iagox86/dnscat2/blob/master/doc/protocol.md#encoding
-               c2c.append(pkt['DNSQR'].qname.decode().split(m_domain)[0][18:].replace('.', '').replace('\n', ''))
+               # and https://github.com/iagox86/dnscat2/blob/master/doc/protocol.md#encoding               
+               c2_pkt = pkt['DNSQR'].qname.decode().split(m_domain)[0].replace('.', '').replace('\n', '')
+               # check if msg
+               if bytearray.fromhex(c2_pkt)[2] == msg_type_msg:
+                   c2c.append(c2_pkt[18:])
 
     return c2c
+
+
+def extract_c2c2():
+    p = rdpcap('/workspaces/python-2/suspicious_traffic.pcap')
+    src_h = '192.168.157.144'
+    dst_h = '192.168.157.145'
+    m_domain = 'microsofto365.com'
+
+
+    msg_type_msg = 1 
+
+    pkt = p[48479]
+    raw_c2c = []
+    
+    return raw_c2c
+
+
+
 
 def decode_c2c(c2c):
     d_c2c = ""
@@ -36,10 +58,18 @@ def decode_c2c(c2c):
     return d_c2c
 
 def main():
-    args = get_args()
+    #args = get_args()
     
-    c2c = extract_c2c(args.source_host, args.target_host, args.domain, args.file)
-    with open(args.output_file, 'w') as ofh:
+    #c2c = extract_c2c(args.source_host, args.target_host, args.domain, args.file)
+ 
+    p = '/workspaces/python-2/suspicious_traffic.pcap'
+    src_h = '192.168.157.144'
+    dst_h = '192.168.157.145'
+    m_domain = 'microsofto365.com'
+    
+    
+    c2c = extract_c2c(src_h, dst_h, m_domain, p)
+    with open('out', 'w') as ofh:
         ofh.write(decode_c2c(c2c))
     
     
