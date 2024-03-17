dd214ee924968edd33ef043cea60f82b83746f95
chris <chris@local.lan>
2024-03-17 17:53:24 +0000

    typo; cleanup

--- dnscat2_decoder/dnscat2_decoder.py
+++ dnscat2_decoder/dnscat2_decoder.py
@@ -12,13 +12,12 @@ def get_args():
     parser.add_argument('-o','--output_file', help='File to write decoded traffic to', nargs='?', required=True)
     return parser.parse_args()
 
-def extract_c2c(src_h, dst_h, m_domain, pcap_file):
+def extract_msg(src_h, dst_h, m_domain, pcap_file):
     capture = PcapNgReader(pcap_file)
     msg_type_msg = 1
-    
     c2c = []
     for pkt in capture:
-        # Pkt is qury between infected host and mal server
+        # Pkt is query between infected host and mal server
         if pkt.haslayer('IP') and src_h == pkt['IP'].src  and dst_h == pkt['IP'].dst and pkt.haslayer('DNS'):
             # Domain is malicious domain
             if m_domain in pkt['DNSQR'].qname.decode():
@@ -26,31 +25,13 @@ def extract_c2c(src_h, dst_h, m_domain, pcap_file):
                # see: https://github.com/iagox86/dnscat2/blob/master/doc/protocol.md#messages
                # and https://github.com/iagox86/dnscat2/blob/master/doc/protocol.md#encoding               
                c2_pkt = pkt['DNSQR'].qname.decode().split(m_domain)[0].replace('.', '').replace('\n', '')
-               # check if msg
+               # check if msg, we only care about msg
+               # https://github.com/iagox86/dnscat2/blob/master/doc/protocol.md#message_type_msg-0x01
                if bytearray.fromhex(c2_pkt)[2] == msg_type_msg:
                    c2c.append(c2_pkt[18:])
-
     return c2c
 
-
-def extract_c2c2():
-    p = rdpcap('/workspaces/python-2/suspicious_traffic.pcap')
-    src_h = '192.168.157.144'
-    dst_h = '192.168.157.145'
-    m_domain = 'microsofto365.com'
-
-
-    msg_type_msg = 1 
-
-    pkt = p[48479]
-    raw_c2c = []
-    
-    return raw_c2c
-
-
-
-
-def decode_c2c(c2c):
+def decode_msg(c2c):
     d_c2c = ""
     for c in c2c:
         d_c2c += binascii.unhexlify(c).decode('utf-8', errors='ignore')
@@ -58,20 +39,10 @@ def decode_c2c(c2c):
     return d_c2c
 
 def main():
-    #args = get_args()
-    
-    #c2c = extract_c2c(args.source_host, args.target_host, args.domain, args.file)
- 
-    p = '/workspaces/python-2/suspicious_traffic.pcap'
-    src_h = '192.168.157.144'
-    dst_h = '192.168.157.145'
-    m_domain = 'microsofto365.com'
-    
-    
-    c2c = extract_c2c(src_h, dst_h, m_domain, p)
+    args = get_args()
+    c2c = extract_msg(args.source_host, args.target_host, args.domain, args.file)
     with open('out', 'w') as ofh:
-        ofh.write(decode_c2c(c2c))
-    
+        ofh.write(decode_msg(c2c))
     
 if __name__ == '__main__':
     main()
